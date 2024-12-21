d6512560eb8f8eff5765bf843fd1d9096d6dd54b
Bernardo Alves <bernardojob2003@outlook.com>
2024-12-21 14:06:18 -0300

    feat(FunctionalInterfaceBinaryOperator): classe responsável por apresentar a utilização do binary operator para redução de coleção/combinação de uma coleção

--- src/main/java/br/com/stream/api/Main.java
+++ src/main/java/br/com/stream/api/Main.java
@@ -1,13 +1,7 @@
 package br.com.stream.api;
 
-import br.com.stream.api.consumer.FunctionalInterfaceConsumer;
-
 public class Main {
     public static void main(String[] args) {
-        FunctionalInterfaceConsumer functionalInterfaceConsumer = new FunctionalInterfaceConsumer();
 
-        functionalInterfaceConsumer.imprimirNumerosPares();
-        
-        functionalInterfaceConsumer.imprimirNumerosImpares();
     }
 }
